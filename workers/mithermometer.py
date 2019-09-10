669e00c19f77d9a21284d99aa17e0913fd38ba21
haiyang.song <songhaiyang@shuwen.com>
2019-09-10 15:22:27 +0800

    add mqtt save

--- workers/mithermometer.py
+++ workers/mithermometer.py
@@ -11,67 +11,70 @@ _LOGGER = logger.get(__name__)
 
 
 class MithermometerWorker(BaseWorker):
-  def _setup(self):
-    from mithermometer.mithermometer_poller import MiThermometerPoller
-    from btlewrap.bluepy import BluepyBackend
+    def _setup(self):
+        from mithermometer.mithermometer_poller import MiThermometerPoller
+        from btlewrap.bluepy import BluepyBackend
 
-    _LOGGER.info("Adding %d %s devices", len(self.devices), repr(self))
-    for name, mac in self.devices.items():
-      _LOGGER.debug("Adding %s device '%s' (%s)", repr(self), name, mac)
-      self.devices[name] = {"mac":mac, "poller": MiThermometerPoller(mac, BluepyBackend)}
+        _LOGGER.info("Adding %d %s devices", len(self.devices), repr(self))
+        for name, mac in self.devices.items():
+            _LOGGER.debug("Adding %s device '%s' (%s)", repr(self), name, mac)
+            self.devices[name] = {"mac": mac, "poller": MiThermometerPoller(mac, BluepyBackend)}
 
-  def config(self):
-    ret = []
-    for name, data in self.devices.items():
-      ret += self.config_device(name, data["mac"])
-    return ret
+    def config(self):
+        ret = []
+        for name, data in self.devices.items():
+            ret += self.config_device(name, data["mac"])
+        return ret
 
-  def config_device(self, name, mac):
-    ret = []
-    device = {
-      "identifiers": [mac, self.format_discovery_id(mac, name)],
-      "manufacturer": "Xiaomi",
-      "model": "LYWSD(CGQ/01ZM)",
-      "name": self.format_discovery_name(name)
-    }
+    def config_device(self, name, mac):
+        ret = []
+        device = {
+            "identifiers": [mac, self.format_discovery_id(mac, name)],
+            "manufacturer": "Xiaomi",
+            "model": "LYWSD(CGQ/01ZM)",
+            "name": self.format_discovery_name(name)
+        }
 
-    for attr in monitoredAttrs:
-      payload = {
-        "unique_id": self.format_discovery_id(mac, name, attr),
-        "name": self.format_discovery_name(name, attr),
-        "state_topic": self.format_prefixed_topic(name, attr),
-        "device_class": attr,
-        "device": device
-      }
+        for attr in monitoredAttrs:
+            payload = {
+                "unique_id": self.format_discovery_id(mac, name, attr),
+                "name": self.format_discovery_name(name, attr),
+                "state_topic": self.format_prefixed_topic(name, attr),
+                "device_class": attr,
+                "device": device
+            }
 
-      if attr == 'temperature':
-        payload["unit_of_measurement"] = "°C"
-      elif attr == 'humidity':
-        payload["unit_of_measurement"] = "%"
-      elif attr == 'battery':
-        payload["unit_of_measurement"] = "%"
+            if attr == 'temperature':
+                payload["unit_of_measurement"] = "°C"
+            elif attr == 'humidity':
+                payload["unit_of_measurement"] = "%"
+            elif attr == 'battery':
+                payload["unit_of_measurement"] = "%"
 
-      ret.append(MqttConfigMessage(MqttConfigMessage.SENSOR, self.format_discovery_topic(mac, name, attr), payload=payload))
+            ret.append(
+                MqttConfigMessage(MqttConfigMessage.SENSOR, self.format_discovery_topic(mac, name, attr), payload=payload, retain=True))
 
-    return ret
+        return ret
 
-  def status_update(self):
-    _LOGGER.info("Updating %d %s devices", len(self.devices), repr(self))
-    ret = []
-    for name, data in self.devices.items():
-      _LOGGER.debug("Updating %s device '%s' (%s)", repr(self), name, data["mac"])
-      from btlewrap import BluetoothBackendException
-      try:
-        ret += self.update_device_state(name, data["poller"])
-      except BluetoothBackendException as e:
-        logger.log_exception(_LOGGER, "Error during update of %s device '%s' (%s): %s", repr(self), name, data["mac"], type(e).__name__, suppress=True)
-      except TimeoutError as e:
-        logger.log_exception(_LOGGER, "Time out during update of %s device '%s' (%s): %s", repr(self), name, data["mac"], type(e).__name__, suppress=True)
-    return ret
+    def status_update(self):
+        _LOGGER.info("Updating %d %s devices", len(self.devices), repr(self))
+        ret = []
+        for name, data in self.devices.items():
+            _LOGGER.debug("Updating %s device '%s' (%s)", repr(self), name, data["mac"])
+            from btlewrap import BluetoothBackendException
+            try:
+                ret += self.update_device_state(name, data["poller"])
+            except BluetoothBackendException as e:
+                logger.log_exception(_LOGGER, "Error during update of %s device '%s' (%s): %s", repr(self), name, data["mac"],
+                                     type(e).__name__, suppress=True)
+            except TimeoutError as e:
+                logger.log_exception(_LOGGER, "Time out during update of %s device '%s' (%s): %s", repr(self), name, data["mac"],
+                                     type(e).__name__, suppress=True)
+        return ret
 
-  def update_device_state(self, name, poller):
-    ret = []
-    poller.clear_cache()
-    for attr in monitoredAttrs:
-      ret.append(MqttMessage(topic=self.format_topic(name, attr), payload=poller.parameter_value(attr)))
-    return ret
+    def update_device_state(self, name, poller):
+        ret = []
+        poller.clear_cache()
+        for attr in monitoredAttrs:
+            ret.append(MqttMessage(topic=self.format_topic(name, attr), payload=poller.parameter_value(attr), retain=True))
+        return ret
